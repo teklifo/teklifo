642a3797952f1d98d815368a79a74d57706789a2
Kamran <kamranv21@gmail.com>
2024-03-02 23:43:14 +0400

    new rfq form

---
+++ src/app/[locale]/(main)/_components/rfq/rqf-form.tsx
@@ -0,0 +1,183 @@
+"use client";
+
+import { useState } from "react";
+import { useTranslations } from "next-intl";
+import { getCookie } from "cookies-next";
+import { zodResolver } from "@hookform/resolvers/zod";
+import type { Prisma } from "@prisma/client";
+import { useForm, useFieldArray } from "react-hook-form";
+import * as z from "zod";
+import { Plus, Trash } from "lucide-react";
+// import AvailableData from "./available-data";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { getRFQSchema } from "@/lib/schemas";
+import request from "@/lib/request";
+
+type RFQType = Prisma.RequestForQuotationGetPayload<{
+  include: { products: true };
+}>;
+
+type RFQFormProps = {
+  rfq?: RFQType;
+};
+
+const RFQForm = ({ rfq }: RFQFormProps) => {
+  const t = useTranslations("RFQForm");
+
+  const update = rfq !== undefined;
+
+  const { toast } = useToast();
+  const [loading, setLoading] = useState(false);
+
+  const st = useTranslations("Schemas.rfqSchema");
+  const formSchema = getRFQSchema(st);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      publicRequest: rfq?.publicRequest,
+      currency: rfq?.currency,
+      startDate: rfq?.startDate,
+      endDate: rfq?.endDate,
+      description: rfq?.description,
+      deliveryAddress: rfq?.deliveryAddress,
+      deliveryTerms: rfq?.deliveryTerms,
+      paymentTerms: rfq?.paymentTerms,
+      products: rfq?.products.map((product) => {
+        return {
+          productId: product.productId ?? undefined,
+          quantity: product.quantity.toNumber(),
+          price: product.price.toNumber(),
+          deliveryDate: product.deliveryDate,
+          comment: product.comment,
+        };
+      }),
+    },
+  });
+
+  const products = useFieldArray({
+    control: form.control,
+    name: "products",
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setLoading(true);
+
+    const config = {
+      method: update ? "put" : "post",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept-Language": getCookie("NEXT_LOCALE"),
+      },
+      body: JSON.stringify(values),
+    };
+
+    try {
+      if (update) {
+        await request<RFQType>(`/api/rfq/${rfq.id}`, config);
+
+        toast({
+          title: t("rfqIsUpdated"),
+          description: t("rfqIsUpdatedHint"),
+        });
+      } else {
+        await request<RFQType>(`/api/rfq/`, config);
+
+        toast({
+          title: t("newRFQIsCreated"),
+          description: t("newRFQHint"),
+        });
+      }
+
+      window.location.href = `/outgoing-rfq`;
+    } catch (error) {
+      let message = "";
+      if (error instanceof Error) message = error.message;
+      else message = String(error);
+      toast({
+        title: update ? t("updateError") : t("error"),
+        description: message,
+        variant: "destructive",
+      });
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="currency"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t("currency")}</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {products.fields.map((productField, index) => (
+          <Card key={productField.id} className="h-full w-full">
+            <CardHeader className="flex flex-row items-center space-x-8">
+              <Button
+                type="button"
+                variant="outline"
+                onClick={() => products.remove(index)}
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </CardHeader>
+            <CardContent>
+              {/* <AvailableData
+                stocks={stocks}
+                priceTypes={priceTypes}
+                index={index}
+              /> */}
+            </CardContent>
+          </Card>
+        ))}
+        <div className="flex flex-col items-start space-y-8">
+          <div className="flex flex-col space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+            <Button
+              type="button"
+              variant="outline"
+              className="space-x-2"
+              onClick={() =>
+                products.append({
+                  productId: 0,
+                  quantity: 0,
+                  price: 0,
+                  deliveryDate: new Date(),
+                  comment: "",
+                })
+              }
+            >
+              <Plus />
+              <span>{t("addRow")}</span>
+            </Button>
+          </div>
+          <Button type="submit" disabled={loading}>
+            {update ? t("update") : t("create")}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default RFQForm;
---
+++ src/app/[locale]/(main)/new-rqf/page.tsx
@@ -0,0 +1,47 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getTranslations } from "next-intl/server";
+import type {
+  Stock as StockType,
+  PriceType as PriceTypeType,
+} from "@prisma/client";
+import RFQForm from "@/app/[locale]/(main)/_components/rfq/rqf-form";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { getStocksAndPriceTypes } from "@/app/actions/get-stocks-price-types";
+import getCurrentCompany from "@/app/actions/get-current-company";
+
+type Props = {
+  params: { locale: string };
+};
+
+export const generateMetadata = async ({
+  params: { locale },
+}: Props): Promise<Metadata> => {
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+
+  return {
+    title: t("newRFQTitle"),
+    description: t("newRFQDescription"),
+  };
+};
+
+const NewRFQ = async () => {
+  const company = await getCurrentCompany();
+  if (!company) return notFound();
+
+  const t = await getTranslations("RFQForm");
+
+  return (
+    <MaxWidthWrapper className="my-8">
+      <div className="space-y-2 mb-4">
+        <h1 className="text-4xl font-bold tracking-tight">
+          {t("newRFQTitle")}
+        </h1>
+        <p className="text-lg text-muted-foreground">{t("newRFQSubtitle")}</p>
+      </div>
+      <RFQForm />
+    </MaxWidthWrapper>
+  );
+};
+
+export default NewRFQ;
