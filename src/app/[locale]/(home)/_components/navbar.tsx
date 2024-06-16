08ec967c99eef2625a201080d11d004f89c0d11c
Kamran <kamranv21@gmail.com>
2024-06-16 21:44:55 +0400

    home page

--- src/app/[locale]/(home)/_components/navbar.tsx
+++ src/app/[locale]/(home)/_components/navbar.tsx
@@ -3,8 +3,8 @@ import Logo from "@/components/logo";
 
 const Navbar = async () => {
   return (
-    <div className="sticky z-50 top-0 inset-x-0 h-16 border-b">
-      <header className="relative bg-background border-b">
+    <div className="sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-background">
         <MaxWidthWrapper>
           <div className="flex h-16 justify-between items-center">
             <Logo />
--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,12 +1,27 @@
+import Image from "next/image";
 import type { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import { Link } from "@/navigation";
-import { HeroHighlight, Highlight } from "./_components/hero-highlight";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import {
+  Coins,
+  LineChart,
+  RefreshCcw,
+  HandCoins,
+  Send,
+  FileStack,
+} from "lucide-react";
 
 type Props = {
   params: { locale: string };
+};
+
+type FeatureProps = {
+  label: string;
+  text: string;
+  icon: React.ComponentType<{ className?: string }>;
 };
 
 export const generateMetadata = async ({
@@ -20,32 +35,95 @@ export const generateMetadata = async ({
   };
 };
 
+const Feature = ({ label, text, icon: Icon }: FeatureProps) => {
+  return (
+    <div className="flex gap-4 items-start">
+      <div className="flex justify-center items-center bg-foreground rounded-full p-2 w-8 h-8 shrink-0">
+        <Icon className="text-background w-5 h-5" />
+      </div>
+      <div className="">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          {label}
+        </h3>
+        <p className="text-xl text-muted-foreground mt-2">{text}</p>
+      </div>
+    </div>
+  );
+};
+
 const Home = async () => {
   const t = await getTranslations("Home");
 
   return (
-    <HeroHighlight>
-      <div className="h-[50vh] w-full px-3 flex flex-col justify-center items-center text-center space-y-8">
-        <h1 className="text-3xl font-bold leading-tight tracking-tighter animate-fade-in max-w-4xl md:text-5xl md:leading-[1.3]">
-          {t.rich("title", {
-            highlight: (chunk) => (
-              <Highlight className="text-black dark:text-white">
-                {chunk}
-              </Highlight>
-            ),
-          })}
-        </h1>
-        <Link
-          href={`/dashboard`}
-          className={cn(
-            buttonVariants({ variant: "default", size: "lg" }),
-            "space-x-2"
-          )}
-        >
-          <span>{t("getStarted")}</span>
-        </Link>
+    <MaxWidthWrapper>
+      <div className="grid lg:grid-cols-2 place-items-center pt-8 pb-16 md:pt-12 md:pb-24">
+        <div>
+          <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+            {t("title")}
+          </h1>
+          <p className="text-xl text-muted-foreground mt-4">{t("subtitle")}</p>
+          <Link
+            href={`/dashboard`}
+            className={cn(
+              buttonVariants({ variant: "default", size: "lg" }),
+              "mt-6"
+            )}
+          >
+            <span>{t("getStarted")}</span>
+          </Link>
+        </div>
+        <div>
+          <Image
+            src="/illustrations/check-email.svg"
+            alt="Main image"
+            priority
+            className="py-6 hidden md:block"
+            width={800}
+            height={800}
+          />
+        </div>
       </div>
-    </HeroHighlight>
+      <div className="mt-16 md:mt-0">
+        <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
+          {t("featuresTitle")}
+        </h2>
+        <p className="text-xl text-muted-foreground mt-4 flex">
+          {t("featuresSubtitle")}
+        </p>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 mt-16 mb-24 gap-16">
+        <Feature
+          label={t("featureLabel1")}
+          text={t("featureText1")}
+          icon={Coins}
+        />
+        <Feature
+          label={t("featureLabel2")}
+          text={t("featureText2")}
+          icon={Send}
+        />
+        <Feature
+          label={t("featureLabel3")}
+          text={t("featureText3")}
+          icon={FileStack}
+        />
+        <Feature
+          label={t("featureLabel4")}
+          text={t("featureText4")}
+          icon={LineChart}
+        />
+        <Feature
+          label={t("featureLabel5")}
+          text={t("featureText5")}
+          icon={HandCoins}
+        />
+        <Feature
+          label={t("featureLabel6")}
+          text={t("featureText6")}
+          icon={RefreshCcw}
+        />
+      </div>
+    </MaxWidthWrapper>
   );
 };
 
