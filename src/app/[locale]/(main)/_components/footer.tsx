0425ccd055870b61e7c9b44427eaeb3cd47bae8b
Kamran <kamranv21@gmail.com>
2024-11-13 01:32:40 +0400

    footer

---
+++ src/app/[locale]/(main)/_components/footer.tsx
@@ -0,0 +1,89 @@
+import { useTranslations } from "next-intl";
+import { useSession } from "next-auth/react";
+import { Link } from "@/navigation";
+import { LogIn } from "lucide-react";
+import FooterMenu from "./footer-menu";
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+
+type FooterProps = {
+  isCollapsed: boolean;
+};
+
+const Footer = ({ isCollapsed }: FooterProps) => {
+  const t = useTranslations("Layout");
+  const { status } = useSession();
+  const authenticated = status === "authenticated";
+
+  return isCollapsed ? (
+    <div className="mb-6 px-2 flex flex-col justify-center items-center gap-4">
+      {!authenticated && (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="/login"
+              className={cn(
+                buttonVariants({ variant: "outline", size: "icon" }),
+                "h-9 w-9"
+              )}
+            >
+              <LogIn className="h-4 w-4" />
+              <span className="sr-only">{t("login")}</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right" className="flex items-center gap-4">
+            {t("login")}
+          </TooltipContent>
+        </Tooltip>
+      )}
+      <FooterMenu />
+    </div>
+  ) : (
+    <div className="space-y-2">
+      {!authenticated && (
+        <div className="mb-6 px-2">
+          <Card>
+            <CardHeader>
+              <CardTitle className="font-semibold tracking-tight text-base">
+                {t("join")}
+              </CardTitle>
+              <CardDescription>{t("joinHint")}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "default", size: "sm" }),
+                  "w-full"
+                )}
+              >
+                {t("login")}
+              </Link>
+            </CardContent>
+          </Card>
+        </div>
+      )}
+
+      <Separator className="w-full" />
+      <div className="p-3 flex justify-between items-center">
+        <p className="text-sm text-muted-foreground">Teklifo 2024</p>
+        <FooterMenu />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,12 @@
 import { Inter } from "next/font/google";
 import { notFound } from "next/navigation";
-import { NextIntlClientProvider, useMessages } from "next-intl";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { getServerSession } from "next-auth/next";
 import { ThemeProvider } from "@/components/theme-provider";
+import NextAuthProvider from "@/components/next-auth-provider";
 import { Toaster } from "@/components/ui/toaster";
+import authOptions from "@/lib/auth-options";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
@@ -15,32 +19,36 @@ const inter = Inter({ subsets: ["latin"] });
 
 const locales = ["en", "ru"];
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
   params: { locale },
 }: RootLayoutProps) {
   if (!locales.includes(locale as any)) notFound();
 
-  const messages = useMessages();
+  const messages = await getMessages();
+
+  const session = await getServerSession(authOptions);
 
   return (
     <html lang={locale} className="h-full">
       <body
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <main className="relative flex flex-col min-h-screen">
-              <div className="flex-grow flex-1">{children}</div>
-            </main>
-          </ThemeProvider>
-          <Toaster />
-        </NextIntlClientProvider>
+        <NextAuthProvider session={session}>
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <main className="relative flex flex-col min-h-screen">
+                <div className="flex-grow flex-1">{children}</div>
+              </main>
+            </ThemeProvider>
+            <Toaster />
+          </NextIntlClientProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
