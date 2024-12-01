909b9ee462e6e31d9110d66e0bd51030eba2ebc2
Kamran <kamranv21@gmail.com>
2024-12-02 00:03:53 +0400

    email http

--- src/lib/nodemailer/sendEmail.ts
+++ src/lib/nodemailer/sendEmail.ts
@@ -31,3 +31,36 @@ export default async function sendEmail(params: EmailParametersType) {
     html,
   });
 }
+
+export async function sendEmailWithFetch(params: EmailParametersType) {
+  const { emailType, locale, subject, receivers, context } = params;
+  const { text, html } = await emailTemplate(emailType, context, locale);
+
+  const domain = "teklifo.com";
+
+  const url = `https://api.eu.mailgun.net/v3/${domain}/messages`;
+
+  const formData = new URLSearchParams();
+  formData.append("from", process.env.EMAIL_FROM ?? "");
+  formData.append("to[0]", receivers);
+  formData.append("subject", subject);
+  formData.append("text", text);
+  formData.append("html", html);
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          `${process.env.EMAIL_SERVER_API_USER}:${process.env.EMAIL_SERVER_API_KEY}`
+        ).toString("base64"),
+    },
+    body: formData,
+  });
+
+  const responseText = await response.text();
+  if (!response.ok) {
+    throw new Error(`Email error: ${responseText}`);
+  }
+}
